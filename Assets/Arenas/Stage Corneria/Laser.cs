e20474def53c63f05186aed52d8078b68f8181b3
Vennittor <ventor_prime@hotmail.com>
2015-12-12 14:13:07 -0800

    Brought in Corneria + Minor code fixes
    Ben, you needed to reset your timer whenever you 'fire' the LAZORs.
    otherwise they only go once. And I created a variable to store the time
    it should fire so it can be changed in teh inspector, and so that it
    isn't hard coded in.

---
+++ Assets/Arenas/Stage Corneria/Laser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour 
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (Vector3.up * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Arenas/Stage Corneria/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour 
+{
+	public GameObject myPrefab;
+	public float timer = 0f;
+	public float fireTime = 15f;
+	public int counter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer = timer + Time.deltaTime;
+		if (timer >= fireTime) 
+		{
+			Instantiate(myPrefab, gameObject.transform.position, gameObject.transform.rotation);
+			timer = 0f;
+		}
+	}
+}
